834ed9fd01c7cb991bec6d4a0d93fa576be23c90
Alex Pitzel <pitzelalex@gmail.com>
2022-11-01 12:48:17 -0400

    feat: Iteration 4 Race #tie? and #winner methods

--- lib/race.rb
+++ lib/race.rb
@@ -1,15 +1,36 @@
 class Race
   attr_reader :office,
-              :candidates
+              :candidates,
+              :status
 
   def initialize(office)
-    @office = office
+    @office     = office
     @candidates = []
+    @status     = 'open'
   end
 
   def register_candidate!(data)
     candidate = Candidate.new(data)
     @candidates << candidate
     return candidate
+  end
+
+  def open?
+    @status == 'open'
+  end
+
+  def close!
+    @status = 'closed'
+  end
+
+  def tie?
+    top = @candidates.max { |candidate| candidate.votes }
+    list = @candidates.select { |candidate| candidate.votes == top.votes }
+    list.count > 1 
+  end
+
+  def winner 
+    return false if open?
+    @candidates.sort_by { |candidate| -candidate.votes }.first
   end
 end
--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -15,6 +15,10 @@ describe Race do
     it 'can hold an array of candidates' do
       expect(race.candidates).to eq([])
     end
+
+    it 'has a status' do
+      expect(race.status).to eq('open')
+    end
   end
   
   describe '#register_candidate!' do 
@@ -27,6 +31,47 @@ describe Race do
       expect(race.candidates.last).to be_a Candidate
       expect(race.candidates.count).to eq(2)
       expect(race.candidates).to eq([c1, c2])
+    end
+  end
+
+  describe '#open?' do
+    it 'returns a boolean based on status' do
+      expect(race.open?).to be true 
+    end
+  end
+    
+  describe '#close!' do
+    it 'sets the status to closed' do
+      race.close!
+        
+      expect(race.status).to eq('closed')
+      expect(race.open?).to be false
+    end
+  end
+
+  describe 'tie?' do
+    it 'checks for a tie' do
+      c1 = race.register_candidate!({name: "Diana D", party: :democrat})
+      c2 = race.register_candidate!({name: "Roberto R", party: :republican})
+      4.times {c1.vote_for!}
+      3.times {c2.vote_for!}
+      expect(race.tie?).to be false
+      c2.vote_for!
+      expect(race.tie?).to be true
+    end
+  end
+
+  describe '#winner' do
+    it 'checks for a winner' do
+      expect(race.winner).to eq false
+      race.close!
+      c1 = race.register_candidate!({name: "Diana D", party: :democrat})
+      c2 = race.register_candidate!({name: "Roberto R", party: :republican})
+      4.times {c1.vote_for!}
+      4.times {c2.vote_for!}
+      expect(race.winner).to eq(c1)
+      c1.vote_for!
+      expect(race.winner).to eq(c1)
     end
   end
 end
